e6c5fc70d991382de18b5cc7f521da77dd02bca4
keith.lomax <keith.lomax@mri-group.com>
2021-01-02 09:39:38 +0100

    update: added Required attribute to Id, to ensure |null not generated in TypeScript; reformatted code for readability

--- MinWebApp.ServiceModel/Types/Company.cs
+++ MinWebApp.ServiceModel/Types/Company.cs
@@ -9,20 +9,34 @@ namespace MinWebApp.ServiceModel.Types
     public class Company
     {
         [AutoId]
-        [DataMember, PrimaryKey] public Guid Id { get; set; } = Guid.Empty;
+        [DataMember, PrimaryKey, Required] 
+        public Guid Id { get; set; } = Guid.Empty;
 
-        [DataMember, Required] public int Ranking { get; set; } = 0;
-        [DataMember, Required] public string Name { get; set; } = string.Empty;
-        [DataMember] public string? Subname { get; set; } = null;
+        [DataMember, Required] 
+        public int Ranking { get; set; } = 0;
 
-        [DataMember, Required] public Guid CountryId { get; set; } = Guid.Empty;
-        [DataMember, Required] public DateTime StatusDate { get; set; } = DateTime.UtcNow;
+        [DataMember, Required] 
+        public string Name { get; set; } = string.Empty;
 
-        [DataMember] public Guid? TurnoverCurrencyId { get; set; } = null;
-        [DataMember] public decimal? TurnoverAmount { get; set; } = null;
+        [DataMember] 
+        public string? Subname { get; set; } = null;
 
-        [DataMember] public int? EmployeeCount { get; set; } = null;
+        [DataMember, Required] 
+        public Guid CountryId { get; set; } = Guid.Empty;
 
-        [DataMember] public string? Note { get; set; } = null;
+        [DataMember, Required] 
+        public DateTime StatusDate { get; set; } = DateTime.UtcNow;
+
+        [DataMember] 
+        public Guid? TurnoverCurrencyId { get; set; } = null;
+
+        [DataMember] 
+        public decimal? TurnoverAmount { get; set; } = null;
+
+        [DataMember] 
+        public int? EmployeeCount { get; set; } = null;
+
+        [DataMember] 
+        public string? Note { get; set; } = null;
     }
 }
